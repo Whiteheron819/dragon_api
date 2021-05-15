f3523b424ca9189f7f98c9a4a94a21de1954d866
Антон Николаев <whiteheron819@yandex.ru>
2021-05-15 11:32:19 +0300

    Check for already created documents, and hiding info about company

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATE_FORMAT = "%Y-%m-%d"
-# BEGIN_DATE = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime(DATE_FORMAT)
-# END_DATE = datetime.datetime.today().strftime(DATE_FORMAT)
-BEGIN_DATE = '2021-03-19'
-END_DATE = '2021-03-20'
+BEGIN_DATE = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime(DATE_FORMAT)
+END_DATE = datetime.datetime.today().strftime(DATE_FORMAT)
+ALREADY_HAVE_ERROR_MESSAGE = 'Такая смена уже существует!'
 NO_MONEY_MESSAGE = 'Нет продаж по наличным за день'
 SUCCESS_MESSAGE = 'Выгрузка успешна'
 AQSI_URL = f'https://api.aqsi.ru/pub/v2/Receipts?filtered.BeginDate={BEGIN_DATE}&filtered.EndDate={END_DATE}&filtered.Operation=1'
 AQSI_TOKEN = os.getenv('AQSI_TOKEN')
 MOE_DELO_URL = 'https://restapi.moedelo.org/accounting/api/v1/cashier/1/retailRevenue'
+MOE_DELO_DOCS_URL = f'https://restapi.moedelo.org/accounting/api/v1/cashier/1/retailRevenue?afterDate={END_DATE}'
 MOE_DELO_TOKEN = os.getenv('MOE_DELO_TOKEN')
+FIRM_INFO = os.getenv('FIRM_INFO')
 
 
 def get_orders():
@@ -33,16 +34,18 @@ def get_orders():
 
 
 def create_document(day_amount, z_number):
-
+    headers = {
+        "md-api-key": MOE_DELO_TOKEN
+    }
+    day_count = requests.get(MOE_DELO_DOCS_URL, headers=headers).json()['ResourceList'][0]['ZReportNumber']
     if day_amount == 0:
         return NO_MONEY_MESSAGE
+    elif day_count == z_number:
+        return ALREADY_HAVE_ERROR_MESSAGE
     else:
-        headers = {
-            "md-api-key": MOE_DELO_TOKEN
-        }
         document = {
             "DocDate": BEGIN_DATE,
-            "Description": f"Отчёт о продаже на точке Студия старинного танца Хрустальный дракон (ИНН=7804535190) на сумму {day_amount} руб",
+            "Description": f"Отчёт о продаже на точке {FIRM_INFO} на сумму {day_amount} руб",
             "Sum": day_amount,
             "ZReportNumber": z_number
         }

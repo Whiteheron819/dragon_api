600dc7d52c87136b24843f0b62202a9dcbf56a3b
Aleksei Faians <af.git@bsrealm.net>
2022-01-22 18:38:06 +0300

    more error checks; exit on get/post errors

--- main.py
+++ main.py
@@ -30,12 +30,21 @@ MOE_DELO_TOKEN = os.getenv('MOE_DELO_TOKEN')
 
 def requests_get(url, headers):
     for i in range(10):
-        r = requests.get(url, headers=headers)
+        r = requests.get(url=url, headers=headers)
         if r.status_code == 200:
             return r
         sleep(10)
-    logging.info(f'Failed to retrieve URL: {url}; status code: {r.status_code}')
-    return False
+    logging.critical(f'Failed to retrieve URL: {url}; status code: {r.status_code}')
+    exit(1)
+
+
+def requests_post(url, data, headers):
+    for i in range(10):
+        r = requests.post(url=url, data=data, headers=headers)
+        if r.status_code == 200:
+            return True
+    logging.critical(f'Failed to post data to {url}; status code: {r.status_code}')
+    exit(1)
 
 
 def get_orders():
@@ -43,17 +52,15 @@ def get_orders():
     headers = {
         "x-client-key": AQSI_TOKEN
     }
-    r = requests_get(AQSI_URL, headers=headers)
-    if r is not False:
-        rj = r.json()
-        if 'rows' in rj:
-            orders = rj['rows']
-            if isinstance(orders, list) and len(orders) > 0 and 'shiftNumber' in orders[0]:
-                shift_number = orders[0]['shiftNumber']
-                for i in range(len(orders)):
-                    if orders[i]['content']['checkClose']['payments'][0]['acquiringData'] is None:
-                        amount_rub += int(orders[i]['content']['checkClose']['payments'][0]['amount'])
-                return amount_rub, shift_number
+    rj = requests_get(url=AQSI_URL, headers=headers).json()
+    if 'rows' in rj:
+        orders = rj['rows']
+        if isinstance(orders, list) and len(orders) > 0 and 'shiftNumber' in orders[0]:
+            shift_number = orders[0]['shiftNumber']
+            for i in range(len(orders)):
+                if orders[i]['content']['checkClose']['payments'][0]['acquiringData'] is None:
+                    amount_rub += int(orders[i]['content']['checkClose']['payments'][0]['amount'])
+            return amount_rub, shift_number
     return 0, 0
 
 
@@ -63,10 +70,7 @@ def create_document(day_amount, z_number):
     headers = {
         "md-api-key": MOE_DELO_TOKEN
     }
-    r = requests_get(MOE_DELO_DOCS_URL, headers=headers)
-    if r is False:
-        return "Не получилось получить данные Моё Дело"
-    orders = r.json()
+    orders = requests_get(url=MOE_DELO_DOCS_URL, headers=headers).json()
     if orders['TotalCount'] != 0 and orders['ResourceList'][0]['ZReportNumber'] == z_number:
         return ALREADY_HAVE_ERROR_MESSAGE
     else:
@@ -77,7 +81,7 @@ def create_document(day_amount, z_number):
             "ZReportNumber": z_number
         }
         success = SUCCESS_MESSAGE.format(day_amount=day_amount, z_number=z_number)
-        requests.post(MOE_DELO_URL, data=document, headers=headers).json()
+        requests_post(url=MOE_DELO_URL, data=document, headers=headers)
         send_mail(success)
         return success
 
@@ -88,7 +92,6 @@ def send_mail(message):
     msg['From'] = MAIL_FROM
     msg['To'] = MAIL_TO
     msg['Subject'] = EMAIL_SUBJECT
-
     msg.attach(MIMEText(message, 'plain'))
     server = smtplib.SMTP('localhost', 25)
     server.sendmail(msg['From'], msg['To'], msg.as_string())
